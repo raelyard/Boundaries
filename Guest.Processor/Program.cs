4ce3c5f03d47536fbf6a24afc06932579ab34ef0
Dave Rael <dave@raelyard.com>
2019-11-19 17:50:03 -0600

    Message Processor for Guest context

---
+++ Guest.Processor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Guest.Processor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
